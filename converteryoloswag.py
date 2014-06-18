fc7102627a34f52dbe22b84796401782644da02b
willybumbum <aidan.king1999@hotmil.co.uk>
2014-06-18 10:00:02 +0100

    Update converteryoloswag.py

--- converteryoloswag.py
+++ converteryoloswag.py
@@ -1,9 +1,10 @@
 currencies= {
     "Pound Sterling": 1, 
-    "Euro": 1.26, 
-    "US Dollar": 1.7, 
-    "Japanese Yen": 173.01
+    "Euro": 1.2, 
+    "US Dollar": 1.25
+    "Japanese Yen": 171.07
     }
+#This is a dictionary storing the easily changeable exchange rates for each: GBP, EUR, USD and JPY.
 
 short_hand = {
     "GBP": "Pound Sterling",
@@ -11,8 +12,14 @@ short_hand = {
     "USD": "US Dollar",
     "JPY": "Japanese Yen"
     }
-    
+#This is a dictionary showing the programmer which three letter code relates to which currency.
+
+print short_hand
+#This shows the user which currency relates to which three letter code.
+
 c_type1 = raw_input("What Currency are you converting from? GBP/EUR/USD/JPY: ")
+c_type1 = c_type1.upper()
+#This allows the user to input a currency to convert from and stores it as the variable 'c_type1' and then capitalises it for the program to read.
 
 if c_type1 == "GBP" or "EUR" or "USD" or "JPY":
     "You entered %s" %(c_type1)
@@ -21,8 +28,9 @@ else:
     c_type1 = raw_input("Please enter a valid currency to convert from- GBP/EUR/USD/JPY: ")
     
 c_type2 = raw_input("What Currency are you converting to? GBP/EUR/USD/JPY: ")
+c_type2 = c_type2.upper()
 
-if c_type2 != "GBP" or "EUR" or "USD" or "JPY":
+if c_type2 == "GBP" or "EUR" or "USD" or "JPY":
     "You entered %s" %(c_type1)
 else:
     print c_type2, "is not a valid input"
@@ -50,15 +58,16 @@ rate(c_type1, c_type2)
 
 numb1 = float(raw_input("How much %s do you wish to convert? " %c_type1))
 
-def conversion(fromCurr, toCurr, value):
-    if fromCurr == "GBP":
-        answer = value * float(currencies[short_hand[toCurr]])
-        return answer
-    elif toCurr == "GBP":
-        answer = value / float(currencies[short_hand[fromCurr]])
+def conversion(w, x, y):
+    if w == "GBP":
+        z = y * float(currencies[short_hand[x]])
+        return round(z, 2)
+    elif x == "GBP":
+        z = y / float(currencies[short_hand[w]])
+        return round(z, 2)
     else:
-        answer = value / float(currencies[short_hand[fromCurr]])
-        answer = value * float(currencies[short_hand[toCurr]]) 
-        return answer
+        z = y / float(currencies[short_hand[w]])
+        z = z * float(currencies[short_hand[x]])
+        return round(z, 2)
 
-print "%2.f %s is %2.f %s" %(numb1, short_hand[c_type1], conversion(c_type1, c_type2, numb1), short_hand[c_type2])
+print "%.2f %s is %.2f %s" %(numb1, short_hand[c_type1], conversion(c_type1, c_type2, numb1), short_hand[c_type2])
